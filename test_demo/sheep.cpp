0c2625e219e09ad1d3381a46003217cdb77d0359
kangyijie5473 <xiyoulinux.kangyijie@gmail.com>
2019-02-15 16:34:02 +0800

    被追踪程序 内存更新

--- test_demo/sheep.cpp
+++ test_demo/sheep.cpp
@@ -3,11 +3,14 @@
 //
 #include <iostream>
 #include <unistd.h>
+using namespace std;
 int main()
 {
     __asm__ __volatile__("movq $5473, %R15"); // test reg
-    long value = 5473;                        // test mem
-    sleep(100);
+    long value = 1024;                        // test mem
+    cout << &value << endl;
+    sleep(30);
+    printf("%lx", value);
     //__asm__ __volatile__()
 }
 
