5d90298bd5eddf49827856252e5c22879650b63f
kangyijie5473 <xiyoulinux.kangyijie@gmail.com>
2019-05-11 22:28:42 +0800

    将公共参数转移到common.h

---
+++ src/common.h
@@ -0,0 +1,12 @@
+//
+// Created by kang on 19-5-11.
+//
+
+#ifndef SRC_COMMON_H
+#define SRC_COMMON_H
+const int C_OK = 0;
+const int C_ERR = -1;
+const int NOT_ATTACH = -1;
+const char INT_ins = (char)0xcc;
+
+#endif //SRC_COMMON_H
