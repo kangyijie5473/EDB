013ef91bc16742feb02e8efb17d83087158b56bb
kangyijie5473 <xiyoulinux.kangyijie@gmail.com>
2019-05-13 16:30:40 +0800

    继续添加一些提示信息

--- src/Displayer.cpp
+++ src/Displayer.cpp
@@ -14,9 +14,16 @@ void completion(const char *buf, linenoiseCompletions *lc) {
     }
     if (buf[0] == 's') {
         linenoiseAddCompletion(lc, "step");
-
     }
-
+    if (buf[0] == 'p') {
+        linenoiseAddCompletion(lc, "print");
+    }
+    if (buf[0] == 'c') {
+        linenoiseAddCompletion(lc, "continue");
+    }
+    if (buf[0] == 'f') {
+        linenoiseAddCompletion(lc, "finish");
+    }
 }
 // line-edit hint infomation
 std::map<std::string, char *> hint = {
@@ -26,9 +33,14 @@ std::map<std::string, char *> hint = {
         {"p", "rint"},
         {"f", "inish"},
         {"ba", "cktrace"},
+        {"c", "continue"},
         {"step", "  单步运行，每次执行一行代码"},
         {"backtrace", "  栈回朔，用于观察当前函数调用关系"},
-        {"finish", "  单步运行，执行完当前函数剩余的所有代码"}
+        {"finish", "  单步运行，执行完当前函数剩余的所有代码"},
+        {"print", "{variable}  打印变量"},
+        {"info registers", " [name] 查看寄存器中的值"},
+        {"info breakpoints", "  查看断点信息"},
+        {"continue", " 继续执行被调试程序"}
 
 };
 char *hints(const char *buf, int *color, int *bold) {
