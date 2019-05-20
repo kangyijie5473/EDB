74c50222f0ba0dc77312c916baca007bc9b87382
kangyijie5473 <xiyoulinux.kangyijie@gmail.com>
2019-05-20 17:29:42 +0800

    调试核心功能测试程序

--- test_demo/sheep.c
+++ test_demo/sheep.c
@@ -3,11 +3,7 @@
 //
 #include <stdio.h>
 #include <unistd.h>
-int add(int a, int b)
-{
-    return a + b;
-}
-void func(int a)
+void func()
 {
     printf("------step 1------\n");
     printf("------step 2------\n");
@@ -19,9 +15,13 @@ void func(int a)
 
 int main()
 {
-    printf("---手动写数据到R15（5473）---\n");
+    printf("---write data to R15（5473）---\n");
     __asm__ __volatile__("movq $5473, %R15"); // 手动写数据到R15寄存器
-    printf("---写完--------------------\n");
+    printf("---write data to R15 fin------\n");
+    printf("---read data from R14--------\n");
+    long v;
+    __asm__ __volatile__("movq %%R14, %0" : "=r"(v));
+    printf("---read data %ld from R14----\n", v);
     printf("---exam memmory-----------\n");
     long value = 1024;
     printf("address:%ld\n", &value);
@@ -29,7 +29,7 @@ int main()
     printf("now value is %ld\n", value);
     printf("---memory fin-------------\n");
     printf("------backtrace and step--\n");
-    func(3);
+    func();
     printf("--backtrace and step fin--\n");
     printf("------sheep fin-----------\n");
     return 0;
